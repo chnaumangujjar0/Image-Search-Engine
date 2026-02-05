3a7a4bc901231fb79738a2edbc6ae329562e8394
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-02-05 22:11:10 +0500

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Image-Search-Engine
+
+<video src="/Image-Search-Engine/image-search-engine.mp4" controls loop >
+
+
+This is a beginner-friendly image search project built using HTML, CSS, and JavaScript with the Unsplash API. Users can type a keyword to search for images and click the Show More button to load additional images.
+
+The app handles common edge cases such as preventing empty searches and clearing old images when a new keyword is searched, so results do not get mixed. Pagination is reset on every new search to keep the results accurate and easy to understand.
---
+++ script.js
@@ -0,0 +1,55 @@
+let input=document.querySelector(".searchbar input");
+let searchBtn=document.querySelector(".search")
+let showMore=document.querySelector(".btn button")
+let imageContainer=document.querySelector(".images")
+let images=document.querySelectorAll(".images img")
+//===I hide the api key for privacy.So,if you want to run this code then  you can use your own unsplash api====
+let apiKey=""
+let keyWord=""
+let page=1;
+
+
+async function showImages() {
+   try {
+    if(input.value==""){
+      alert("please add keyword")
+      return;
+    }
+    
+    if(keyWord!=input.value){
+       imageContainer.innerHTML="";
+       page=1;
+       
+    }
+     
+     keyWord=input.value
+     let url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyWord}&client_id=${apiKey}&per_page=12`;
+
+     let response=await fetch(url);
+     let data=await response.json();
+     for(let i=0;i<12;i++){
+        let img=document.createElement("img")
+        let link=document.querySelector("a")
+         link=data.results[i].links.html
+        img.src=data.results[i].urls.small;
+        img.href=link;
+        img.target="_BLANK"
+        imageContainer.appendChild(img)
+       }
+       showMore.style.display="block";
+      
+   } catch (error) {
+     console.log(error)
+   }
+}
+
+ searchBtn.addEventListener("click",()=>{ 
+    showImages();
+
+ })
+
+ showMore.addEventListener("click",()=>{
+    page++;
+    
+    showImages();
+ })
